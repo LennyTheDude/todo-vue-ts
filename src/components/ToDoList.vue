ae42f418cc2e6b5cd0a228e81090fbf221c29ee6
Lev Antipenko <lennyfromrussia@gmail.com>
2021-06-11 17:59:27 +0300

    able to add todo

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="to-do-list">
     <div>
+      <input type="text" id="new-todo" v-model="newToDo" />
+      <button @click="clickAdd()">Add</button>
       <ul>
         <li v-for="todo in todolist" :key="todo.message">
           <input type="checkbox" v-model="todo.isDone">
@@ -19,6 +21,18 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class ToDoList extends Vue {
   @Prop() todolist!: Array<any>;
   
+  newToDo = "";
+
+  clickAdd() {
+    if (!this.newToDo) return;
+    this.todolist.push({
+      id: this.todolist[this.todolist.length - 1].id + 1,
+      text: this.newToDo,
+      isDone: false
+    });
+    this.newToDo = "";
+  }
+
   clickDelete(id: number) {
     const index = this.todolist.findIndex(el => id === el.id);    
     this.todolist.splice(index, 1);
